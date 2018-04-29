68c4fbb331e41d79e65c952fc57ae3f5dc442d00
YeonwooSung <neos960518@gmail.com>
2018-04-29 09:47:32 +0900

    Make a little bit of progress in blueberrySemaphore.c file

--- Synchronization/Semaphore/bluberrySemaphore.c
+++ Synchronization/Semaphore/bluberrySemaphore.c
@@ -1,3 +1,27 @@
 /********************************************
  * Created by Yeonwoo Sung on 2018. 4. 27.. *
  ********************************************/
+
+#include "blueberrySemaphore.h"
+
+// Initialize the semaphore.
+int msg_sem_init(int semNum, int s) {
+    semNum += SEMSTART;
+
+    if (semNum > SEMEND || semNum < SEMSTART) {
+        retrun -1; //if the number of the semaphore is out of bound, return -1.
+    }
+
+    msgmng.free_msg_pool[semNum].flag = s;
+
+    return 0;
+}
+
+//Locking function of this semaphore.
+int msg_sem_p(int semNum) {
+    semNum += SEMSTART;
+
+    if (msgmng.free_msg_pool[semNum].flag <= 0) {
+        return -2; //if the flag value of the semaphore variable in the given index is less than 0, return -2.
+    }
+}
--- Synchronization/Semaphore/blueberrySemaphore.h
+++ Synchronization/Semaphore/blueberrySemaphore.h
@@ -5,12 +5,14 @@
 #ifndef BLUEBERRIAN_BLUEBERRYSEMAPHORE_H
 #define BLUEBERRIAN_BLUEBERRYSEMAPHORE_H
 
-#define MAXMSG 255 /* the size of the message area that semaphore and process could use. */
+#define MAXMSG 255 /* the size of the message area that semaphore and process could use. (0 ~ 255) */
 
 #define ITCSTART 0 /* The starting point of the message area that the process could use */
 #define ITCEND 99 /* The end point of the message area that the process could use */
 #define SEMSTART 100 /* The starting point of the message area that the semaphore could use */
 #define SEMEND 199 /* The end point of the message area that the semaphore could use */
+
+// The Blueberrian  could have maximum 100 semaphore variables (100 ~ 199)
 
 typedef struct _blueberrian_free_msg {
     int data;
@@ -37,7 +39,7 @@ int msg_itc_send(int, int*);
 int msg_itc_get(int, int*);
 
 int msg_sem_int(int, int);
-int msg_sem_p(int);
-int msg_sem_v(int);
+int msg_sem_p(int); //Lock the semaphore.
+int msg_sem_v(int); //Increase the counter.
 
 #endif //BLUEBERRIAN_BLUEBERRYSEMAPHORE_H
