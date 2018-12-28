b3b19e4faf6314a501ddd9362df2e5220e3dad5d
YeonwooSung <neos960518@gmail.com>
2018-12-28 14:59:27 +0900

    add strtoul()

--- include/pxa255.h
+++ include/pxa255.h
@@ -513,4 +513,38 @@ typedef void            (*ExcpHndlr) (void);
 #define ICCR		__REG(0x40D00014)  // Interrupt Controller Control Register
 
 
+/**
+ * Convert the string to unsigned long.
+ */
+unsigned long int strtoull(const char *ptr, char **end, int base) {
+	unsigned long ret = 0;
+
+	if (base > 36)
+		goto out;
+
+	while (*ptr) {
+		int digit;
+
+		if (*ptr >= '0' && *ptr <= '9' && *ptr < '0' + base)
+			digit = *ptr - '0';
+		else if (*ptr >= 'A' && *ptr < 'A' + base - 10)
+			digit = *ptr - 'A' + 10;
+		else if (*ptr >= 'a' && *ptr < 'a' + base - 10)
+			digit = *ptr - 'a' + 10;
+		else
+			break;
+
+		ret *= base;
+		ret += digit;
+		ptr++;
+	}
+
+    out:
+	if (end)
+		*end = (char *)ptr;
+
+	return ret;
+}
+
+
 #endif //BLUEBERRIAN_PXA255_H
